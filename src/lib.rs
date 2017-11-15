39e91c016d484c79a164abba2718d53ecd2674c2
Johann Tuffe <tafia973@gmail.com>
2017-11-15 23:39:05 +0800

    start page reading

--- src/lib.rs
+++ src/lib.rs
@@ -5,27 +5,31 @@
 #[macro_use] extern crate error_chain;
 extern crate byteorder;
 #[macro_use] extern crate log;
+extern crate encoding_rs;
 
 pub mod errors;
-mod endian;
+mod byte_reader;
 
 use errors::{Result, ErrorKind};
 use std::io::{Read};
-use std::str::from_utf8;
-use endian::Endian;
-
-#[derive(Debug)]
-enum OsType {
-    Unix,
-    Win,
-}
+use byte_reader::ByteReader;
+use encoding_rs::Encoding;
 
 #[derive(Debug)]
 struct Header {
-     endian: Endian,
+     byte_reader: ByteReader,
+     encoding: &'static Encoding,
      page_len: usize,
      page_count: usize,
-     is_64: bool,
+     //platform: Platform
+     offsets: Offsets,
+}
+
+#[derive(Debug)]
+struct Offsets {
+    int: usize,
+    page_start: usize,
+    sub_header_ptr_len: usize,
 }
 
 
@@ -34,11 +38,8 @@ impl Header {
     /// Parses bytes into a new Header
     fn from_reader<R: Read>(read: &mut R) -> Result<Header> {
 
-        // maximum relevant information is 342 bytes
-        let mut buf = [0u8; 342];
-
-        // first chunk of data, alignment free
-        read.read_exact(&mut buf[0..164])?;
+        let mut buf = [0u8; 1024];
+        read.read_exact(&mut buf[0..1024])?;
         
         // magic number
         if &buf[..32] != &[
@@ -50,53 +51,67 @@ impl Header {
         }
 
         // alignments
-        let a2 = if buf[32] == 0x33 { 4 } else { 0 };
-        let a1 = if buf[35] == 0x33 { 4 } else { 0 };
-
-        // endianness
-        let endian = Endian::from_bool(buf[37] == 0x00);
-
-        // os type
-        let os_type = match buf[39] {
-            0x01 => OsType::Unix,
-            0x02 => OsType::Win,
-            t => bail!(ErrorKind::OsType(t)),
+        let offsets = if buf[32] == 0x33 {
+            Offsets {
+                int: 4,
+                page_start: 32,
+                sub_header_ptr_len: 24,
+            }
+        } else {
+            Offsets {
+                int: 0,
+                page_start: 16,
+                sub_header_ptr_len: 12,
+            }
         };
-        debug!("{:?}", os_type);
+        let a1 = if buf[35] == 0x33 { 4 } else { 0 };
+        debug!("alignments: a1 {}, a2 {}", a1, offsets.int);
+
+        // endianness and pointer size
+        let byte_reader = ByteReader::from_bool(buf[37] == 0x01, offsets.int == 4);
 
         if &buf[84..92] != b"SAS FILE" {
             bail!(ErrorKind::Invalid("SAS FILE"));
         }
 
-        let dataset_name = from_utf8(&buf[92..156])?;
-        let file_type = from_utf8(&buf[156..164])?;
+        let encoding = Encoding::for_label(match buf[70] {
+            29 => b"latin1",
+            20 => b"utf-8",
+            33 => b"cyrillic",
+            60 => b"wlatin2",
+            61 => b"wcyrillic",
+            62 => b"wlatin1",
+            90 => b"ebcdic870",
+            v => bail!("Unknown encoding {}", v),
+        }).unwrap_or(::encoding_rs::UTF_8);
 
-        let header_len = endian.read_i32(&buf[196 + a1..200 + a1]);
+        let dataset_name = encoding.decode(&buf[92..156]).0;
+        let file_type = encoding.decode(&buf[156..164]).0;
+        debug!("{} {}", dataset_name, file_type);
+
+        let header_len = byte_reader.read_i32(&buf[(196 + a1)..(200 + a1)]);
         match header_len {
-            1024 | 8192 => (),
-            l => bail!(ErrorKind::Invalid("Header Length")),
+            1024 => (),
+            8192 => {
+                // exhaust header
+                let mut remaining = [0; 8192 - 1024];
+                read.read_exact(&mut remaining)?;
+            }
+            l => bail!(format!("Invalid header: {}", l)),
         }
 
-        let page_len = endian.read_i32(&buf[200 + a1..204 + a1]) as usize;
-        let page_count = if a2 == 0 {
-            endian.read_i32(&buf[204 + a1..208 + a1 + a2]) as usize
-        } else {
-            endian.read_i64(&buf[204 + a1..208 + a1 + a2]) as usize
-        };
-
-        // exhaust header
-        let mut remaining = vec![0; header_len as usize - 342];
-        read.read_exact(&mut remaining)?;
+        let page_len = byte_reader.read_i32(&buf[200 + a1..204 + a1]) as usize;
+        let page_count = byte_reader.read_usize(&buf[204 + a1..208 + a1 + offsets.int]);
 
         Ok(Header {
-            endian: endian,
+            byte_reader: byte_reader,
+            encoding: encoding,
             page_len: page_len,
             page_count: page_count,
-            is_64: a2 == 4,
+            offsets: offsets,
         })
     }
 }
-
 
 /// A sas7bdat reader
 pub struct Reader<R> {
@@ -104,30 +119,65 @@ pub struct Reader<R> {
     inner: R,
     /// The file header
     header: Header,
+    /// current page
+    page_num: usize,
 }
 
 impl<R: Read> Reader<R> {
 
     /// Creates a new sas7bdat `Reader` out of a regular reader
+    ///
+    /// Initialize by reading the header
     pub fn from_reader(mut reader: R) -> Result<Self> {
-
         let header = Header::from_reader(&mut reader)?;
-
         Ok(Reader {
             inner: reader,
             header: header,
+            page_num: 0,
         })
+    }
+
+    /// Reads the next page
+    pub fn next_page(&mut self) -> Result<Option<Page>> {
+        if self.page_num == self.header.page_count {
+            return Ok(None);
+        }
+
+        // fill buffer
+        let mut buf = vec![0u8; self.header.page_len];
+        self.inner.read_exact(&mut buf)?;
+
+        let byte_reader = &self.header.byte_reader;
+        let start = self.header.offsets.page_start;
+        let page_type = byte_reader.read_u16(&buf[start..]);
+
+        let page = Page {};
+        self.page_num += 1;
+        Ok(Some(page))
     }
 
 }
 
+/// A page of data
+pub struct Page {
 
+}
 
 
 #[cfg(test)]
 mod tests {
+    use std::fs::File;
+    use std::io::{BufReader};
+    use super::*;
+
+    fn get_file() -> BufReader<File> {
+        BufReader::new(File::open("olympic.sas7bdat").unwrap())
+    }
+
     #[test]
     fn it_works() {
+        let mut reader = Reader::from_reader(get_file()).unwrap();
+        println!("{:?}", reader.header);
         assert_eq!(2 + 2, 4);
     }
 }
